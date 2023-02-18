f5531b7a6807db2aa608a4410b8f90cb4321a1e4
jacob.toString() <34689611+yaacobmartinez@users.noreply.github.com>
2023-02-18 18:47:14 +0800

    Created quiz config

---
+++ src/assets/quiz.js
@@ -0,0 +1,89 @@
+export const quizDetails = {
+    pointsPerQuestion: 2,
+    recommendations: [
+      {
+        name: 'Low Score',
+        score: 0,
+        recommendation: ['B2 First', 'C1 Advanced'],
+      },
+      {
+        name: 'Average Score',
+        score: 6,
+        recommendation: ['B2 First for Schools'],
+      },
+      {
+        name: 'High Score',
+        score: 12,
+        recommendation: ['B1 Preliminary for Schools', 'A2 Key for Schools'],
+      },
+    ],
+    courses: [
+      {
+        name: 'B2 First', 
+        image: 'https://images.ctfassets.net/dzoh37iwqged/3v6nY65prU95e83gL2iypt/3fe1d42ed3033c988143a8c2577cd77d/B2_First_-_MockUp.png?fm=webp',
+        subtext: '18 months of access - No monthly charges'
+      },
+      {
+        name: 'C1 Advanced', 
+        image: 'https://images.ctfassets.net/dzoh37iwqged/3iR2cmTkDTl1WZ5p8eR9uW/94c3a85c4d150325d49f276f947764e7/C1_-_Tablet_and_Mobile.png?fm=webp',
+        subtext: '18 months of access - No monthly charges'
+      },
+      {
+        name: 'B2 First for Schools', 
+        image: 'https://images.ctfassets.net/dzoh37iwqged/21imErHknUIHxXc9jnxqow/fbfc458db2d2798a958cb043a5ff6efc/B2_first_for_schools_-_MockUp.png?fm=webp',
+        subtext: '18 months of access - No monthly charges'
+      },
+      {
+        name: 'B1 Preliminary for Schools', 
+        image: 'https://images.ctfassets.net/dzoh37iwqged/47LonbGODPADpjNXY2M0u2/9787963a0e893d8c1e15330c7b2db538/B1_Preliminary_for_schools_-_MockUp.png?fm=webp',
+        subtext: '18 months of access - No monthly charges'
+      },
+      {
+        name: 'A2 Key for Schools', 
+        image: 'https://images.ctfassets.net/dzoh37iwqged/46pQKnxiegvb9iFe0qkyMz/cc09723a707c2f8df879521fbdd9f265/A2_Key_for_schools_-_MockUp.png?fm=webp',
+        subtext: '18 months of access - No monthly charges'
+      },
+    ],
+    questions: [
+      {
+        question: 'Juan was a very competent manager, but he _______ with time-keeping.',
+        choices: ['interfered', 'struggled', 'collided', 'clashed'],
+        answer: 'struggled'
+      },
+      {
+        question: 'Many people are surprised by differences between their mental _____ of their face and what they see in photographs.',
+        choices: ['interpretation', 'design', 'illustration', 'image'],
+        answer: 'image'
+      },
+      {
+        question: 'The most prevalent of the many ______ the doctors saw in the East Africa Clinic were parasitic and respiratory system infections.',
+        choices: ['grievances', 'afflications', 'burdens', 'troubles'],
+        answer: 'afflications'
+      },
+      {
+        question: 'People were terrified of her dog because his fluffy coat made him look enormous, but he was really just enthusiastic rather than _______.',
+        choices: ['assertive', 'aggressive', 'sullen', 'surly'],
+        answer: 'aggressive'
+      },
+      {
+        question: 'When I broke my leg I was stuck inside for weeks, and the days just ______ so slowly.',
+        choices: ['got lost', 'kept on', 'went by', 'took away'],
+        answer: 'went by'
+      },
+      {
+        question: 'All of her friends admired her ______ because she always looked so well-groomed and perfectly dressed. ',
+        choices: ['appearance', 'fashion', 'expression', 'example'],
+        answer: 'appearance'
+      },
+      {
+        question: 'Ashley is regarded as quite ______, greeting every challenge with enthusiasm and excitement. ',
+        choices: ['comforting', 'tranquil', 'assertive', 'cheerful'],
+        answer: 'cheerful'
+      },
+      {
+        question: 'Can you take it to the ______ ? Peter is in there, cooking dinner. ',
+        choices: ['breakfast nook', 'dining room', 'sitting room', 'kitchen'],
+        answer: 'kitchen'
+      },
+    ],
+  }
